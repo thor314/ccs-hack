4f186cc8e00d2bbf19f0c86bdeaff22497c2c57a
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-05-12 14:32:34 +0200

    :art: finite fields

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,10 +1,4 @@
 // Import necessary libraries
-use ndarray::{Array, Array2}; // For matrix and vector operations
-use num_bigint::BigUint; // For finite field operations
 pub mod ccs; // For the CCS structure
 pub mod r1cs; // For the R1CS structure
-// We need to define our own Finite Field type.
-#[derive(Debug, Clone)]
-pub struct FiniteField {
-    p: BigUint,
-}
+pub mod FiniteField;
