dc0834ce5f54dfcc8e12a182f81aee6383c39fe2
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-05-12 16:17:46 +0200

    :fire: Customized constrain system

--- src/types/ccs.rs
+++ src/types/ccs.rs
@@ -1,4 +1,23 @@
-// Defining the CCS structure
+use ndarray::{Array, Array2}; // For matrix and vector operations
+use std::fmt;
+
+#[derive(Debug)]
+pub struct CCSError {
+    details: String
+}
+
+impl CCSError {
+    fn new(msg: &str) -> CCSError {
+        CCSError{details: msg.to_string()}
+    }
+}
+
+impl fmt::Display for CCSError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.details)
+    }
+}
+
 pub struct CCS {
     m: usize,
     n: usize,
@@ -8,11 +27,10 @@ pub struct CCS {
     q: usize,
     d: usize,
     M: Vec<Array2<FiniteField>>,
-    S: Vec<Vec<usize>>, // Multisets are represented as Vecs
+    S: Vec<Vec<usize>>,
     c: Vec<FiniteField>,
 }
 
-// Defining the CCS instance and witness
 pub struct CCSInstance {
     x: Array<FiniteField>,
 }
@@ -20,9 +38,38 @@ pub struct CCSInstance {
 pub struct CCSWitness {
     w: Array<FiniteField>,
 }
+
 impl CCS {
-    pub fn is_satisfied_by(&self, instance: &CCSInstance, witness: &CCSWitness) -> bool {
-        // Implement the checks based on the equation (2) in the definition
-        // This will involve matrix-vector multiplication, Hadamard product, and summing over the multisets
+    pub fn is_satisfied_by(&self, instance: &CCSInstance, witness: &CCSWitness) -> Result<bool, CCSError> {
+        if self.n < self.l {
+            return Err(CCSError::new("n must be greater than l"));
+        }
+
+        if self.M.len() != self.t {
+            return Err(CCSError::new("M must have length t"));
+        }
+
+        if self.S.len() != self.q {
+            return Err(CCSError::new("S must have length q"));
+        }
+
+        if self.c.len() != self.q {
+            return Err(CCSError::new("c must have length q"));
+        }
+
+        // Compute z = (w, 1, x)
+        let z = witness.w.clone().into_iter().chain(
+            std::iter::once(FiniteField::one(&self.M[0][[0,0]].p)).chain(instance.x.clone().into_iter())
+        ).collect::<Array<_, _>>();
+
+        // Compute sum from i=0 to q-1 of (ci * sum for each j in Si of Mj * z)
+        let result = (0..self.q).map(|i| {
+            self.c[i] * self.S[i].iter().fold(FiniteField::zero(&self.M[0][[0,0]].p), |sum, &j| {
+                sum + self.M[j].dot(&z)
+            })
+        }).sum::<FiniteField>();
+
+        // Check if result is zero
+        Ok(result.is_zero())
     }
 }
