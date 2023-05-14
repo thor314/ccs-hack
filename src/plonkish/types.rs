c7b268274ed80bdeebf95fb5ae7736d3c5334113
Thor Kamphefner <thorck@pm.me>
2023-05-14 12:53:50 +0200

    plonkish start

--- src/plonkish/types.rs
+++ src/plonkish/types.rs
@@ -1,5 +1,8 @@
 use ark_ff::Field;
 
+/// Each constraint $i$ is specified by a vector $T_i$ of len $t$, with
+/// entries over [n+e-1]. $T_i$ is interpreted as specifying $t$ entries of a purported
+/// satisfying assignment $z$ to feed to $g$.
 #[derive(Clone)]
 pub struct Constraint<F: Field> {
   pub points: Vec<F>,
