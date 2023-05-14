aed85aca3ff4999cf3e400893ca96cfd8c16f071
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-05-14 15:24:42 +0200

    copy constraints + bound checks

--- src/plonkish/mod.rs
+++ src/plonkish/mod.rs
@@ -1,7 +1,9 @@
+use std::collections::HashSet;
+
 use ark_ff::Field;
 use ark_poly::Polynomial;
 
-use self::types::{PlonkishConstraint, PlonkishInstance, PlonkishWitness};
+use self::types::{PlonkishInstance, PlonkishWitness, UncheckedCopyConstaint, UncheckedGateConstratint, PlonkishGateConstraint, PlonkishCopyConstraint};
 
 pub mod types;
 
@@ -23,32 +25,37 @@ pub struct PlonkishStructure<F: Field, P: Polynomial<F, Point = F>> {
   selectors:   Vec<F>,
   /// A set of $m$ constraints. Each constraint is specified via  vector $T_i$ of length $t$ over
   /// [n+e-1].
-  constraints: Vec<PlonkishConstraint<F>>,
+  gate_constraints: Vec<PlonkishGateConstraint<F>>,
+  copy_constraints: Vec<PlonkishCopyConstraint>,
 }
 
 impl<F: Field, P: Polynomial<F, Point = F>> PlonkishStructure<F, P> {
   pub fn new(
-    m: usize,
-    n: usize,
-    l: usize,
-    e: usize,
-    t: usize,
-    q: usize,
-    d: usize,
+    m: usize, // numbers of rows
+    n: usize, // number of columns
+    l: usize, // size of public input (collums)
+    e: usize, // non selector collumns 
+    t: usize, // Number of variables
+    q: usize, // number of terms in plonk equation
+    d: usize, // Max degree
     g: P,
     selectors: Vec<F>,
-    constraints: Vec<Vec<F>>,
+    gate_constraints: Vec<UncheckedGateConstratint<F>>,
+    copy_constaints: Vec<UncheckedCopyConstaint>,
   ) -> Self {
     // todo: assert g has t vars
     // todo assert g is a sum of q monomials
     assert!(g.degree() <= d);
     assert!(selectors.len() == e);
-    assert!(constraints.len() == m);
+    assert!(gate_constraints.len() == m);
     let constraint_point_max = F::from((n + e - 1) as u64);
-    let constraints =
-      constraints.into_iter().map(|c| Self::new_constraint(c, t, constraint_point_max)).collect();
+    let gate_constraints: Vec<PlonkishGateConstraint<F>> =
+      gate_constraints.into_iter().map(|c| Self::new_gate_constraint(c, t, constraint_point_max)).collect();
 
-    Self { m, n, l, e, t, q, d, g, selectors, constraints }
+    let hashSet_copy_constraints: HashSet<_> = copy_constaints.into_iter().map(|c| Self::new_copy_constraint(c, m, n )).collect();
+    let copy_constraints: Vec<PlonkishCopyConstraint> = hashSet_copy_constraints.into_iter().collect();
+    // get this ^ into a vec of copy constraints
+    Self { m, n, l, e, t, q, d, g, selectors, gate_constraints, copy_constraints }
   }
 
   /// A Plonkish structure-instance (S,w) is satisfied by a Plonkish witness $w$ if:
@@ -67,12 +74,20 @@ impl<F: Field, P: Polynomial<F, Point = F>> PlonkishStructure<F, P> {
   }
 
   // todo: panic -> err
-  pub fn new_constraint(t_i: Vec<F>, t: usize, point_max: F) -> PlonkishConstraint<F> {
+  pub fn new_gate_constraint(t_i: Vec<F>, t: usize, point_max: F) -> PlonkishGateConstraint<F> {
     assert_eq!(t_i.len(), t);
     for &point in t_i.iter() {
       assert!(point <= point_max);
     }
-    PlonkishConstraint { points: t_i }
+    PlonkishGateConstraint { points: t_i }
+  }
+  pub fn new_copy_constraint(copy_constraint: UncheckedCopyConstaint ,rows: usize, columns: usize) -> PlonkishCopyConstraint {
+
+    assert!(copy_constraint.0.0 < columns);
+    assert!(copy_constraint.1.0 < columns);
+    assert!(copy_constraint.0.1 < rows);
+    assert!(copy_constraint.1.1 < rows);
+    PlonkishCopyConstraint { points: copy_constraint }
   }
 
   pub fn new_instance(x: Vec<F>, n: usize, l: usize) -> PlonkishInstance<F> {
--- src/plonkish/types.rs
+++ src/plonkish/types.rs
@@ -1,11 +1,16 @@
 use ark_ff::Field;
-
+pub type UncheckedCopyConstaint = ((usize,usize),(usize,usize));
+pub type UncheckedGateConstratint<F> = Vec<F>;
 /// Each constraint $i$ is specified by a vector $T_i$ of len $t$, with
 /// entries over [n+e-1]. $T_i$ is interpreted as specifying $t$ entries of a purported
 /// satisfying assignment $z$ to feed to $g$.
 #[derive(Debug, Clone)]
-pub struct PlonkishConstraint<F: Field> {
+pub struct PlonkishGateConstraint<F: Field> {
   pub points: Vec<F>,
+}
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct PlonkishCopyConstraint {
+  pub points: ((usize,usize),(usize,usize)),
 }
 
 #[derive(Debug, Clone)]
@@ -18,4 +23,15 @@ pub struct PlonkishInstance<F: Field> {
 pub struct PlonkishWitness<F: Field> {
   /// w in %\mathbb F^{n-l}$
   pub w: Vec<F>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Ord)]
+pub struct Point {
+  pub x: usize,
+  pub y: usize,
+}
+impl PartialOrd for Point {
+  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    Some(self.cmp(other))
+  }
 }
