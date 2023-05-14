f8db2fd6dcf9bccaf112e21629cd7a01fa636ad9
Thor <7041313+thor314@users.noreply.github.com>
2023-05-14 14:05:52 +0200

    plonkish2 (#12)
    * plonkish start
    
    * check constraints ploknish
    
    * cf
    
    * check constraints -> is satisfied by
    
    * rm tests

--- src/plonkish/types.rs
+++ src/plonkish/types.rs
@@ -1,6 +1,21 @@
 use ark_ff::Field;
 
-#[derive(Clone)]
-pub struct Constraint<F: Field> {
+/// Each constraint $i$ is specified by a vector $T_i$ of len $t$, with
+/// entries over [n+e-1]. $T_i$ is interpreted as specifying $t$ entries of a purported
+/// satisfying assignment $z$ to feed to $g$.
+#[derive(Debug, Clone)]
+pub struct PlonkishConstraint<F: Field> {
   pub points: Vec<F>,
+}
+
+#[derive(Debug, Clone)]
+pub struct PlonkishInstance<F: Field> {
+  /// w in %\mathbb F^{l}$
+  pub x: Vec<F>,
+}
+
+#[derive(Debug, Clone)]
+pub struct PlonkishWitness<F: Field> {
+  /// w in %\mathbb F^{n-l}$
+  pub w: Vec<F>,
 }
